57d7e054035ac2202d71c8a3e5592fe75354374b
Tommy Tho <tommytho@Tommys-MacBook-Pro.local>
2020-04-03 20:50:37 -0700

    added title component

---
+++ src/components/title/Title.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Styles.css'
+
+function Title() {
+    return (
+        <div className="Title">
+            <a className="Title-subtitle">
+                Hello! I'm
+            </a>
+            <h1 className="Title-header">
+                Tommy Tho
+            </h1>
+            <a className="Title-subheader">
+                I'm a computer science undergrad from Malaysia. I do software development and some graphic design on the side.
+            </a>
+        </div>
+    )
+}
+
+export default Title;
+
+{/* <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+>
+    Learn React
+</a> */}
