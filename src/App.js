533ea7e165487ca2c05cead52ae44f5e3084ac69
Tommy Tho <tommythowm@gmail.com>
2020-08-26 10:56:47 +0800

    completed skills section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Bio from './components/bio/Bio';
 import Contacts from './components/contacts/Contacts';
 import Resumes from './components/resumes/Resumes';
+import Skills from './components/skills/Skills';
 import Title from './components/title/Title';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <header className="App-header">
         <Title />
         <Bio />
+        <Skills />
         <Contacts />
         <Resumes />
       </header>
--- src/components/resumes/Resumes.js
+++ src/components/resumes/Resumes.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './Resumes.css'
 import {ReactComponent as Coop} from './assets/technical-resume-co-op-download.svg'
