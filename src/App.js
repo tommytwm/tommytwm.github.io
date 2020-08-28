86355e0f631e2fee719129a94b91a46ce1fec6ca
Tommy Tho <tommythowm@gmail.com>
2020-08-28 15:30:02 +0800

    completed projects section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Bio from './components/bio/Bio';
 import Contacts from './components/contacts/Contacts';
 import Experience from './components/experience/Experience';
+import Projects from './components/projects/Projects';
 import Resumes from './components/resumes/Resumes';
 import Skills from './components/skills/Skills';
 import Title from './components/title/Title';
@@ -15,6 +16,7 @@ function App() {
         <Bio/>
         <Skills/>
         <Experience />
+        <Projects />
         <Contacts/>
         <Resumes/>
       </header>
