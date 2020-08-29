514649ce092fc0e07ae9e53ae26ef56617c25bbe
Tommy Tho <tommythowm@gmail.com>
2020-08-29 22:14:53 +0800

    added mobile breakpoint for bio section

--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -7,10 +7,10 @@ import Photo from './assets/pic.png';
 function Bio() {
     return (
         <div className="Bio">
+            <h2 className="Bio-header">
+                So this is me 🤗
+            </h2>
             <div className="Bio-text">
-                <h2 className="Bio-header">
-                    So this is me 🤗
-                </h2>
                 <div className="Bio-body">
                     <p>
                         I’m 21 years old, born and raised in Malaysia! I currently reside in <b>Vancouver, BC.</b> I’ve been living in Canada since August 2017 as an international student — and so far, loving the west coast! I’ve made this website to tell my story.
@@ -22,12 +22,12 @@ function Bio() {
                         When I’m not coding, I’m usually out looking for another coffee joint, climbing at the bouldering gym, cooking, tinkering with my PC, designing another website, or just hanging out with my friends. :)
                     </p>
                 </div>
-            </div>
-            <div className="Bio-img">
-                <img
-                    src={Photo}
-                    alt={"Me in Alberta, during Summer of 2019"}
-                />
+                <div className="Bio-img">
+                    <img
+                        src={Photo}
+                        alt={"Me in Alberta, during Summer of 2019"}
+                    />
+                </div>
             </div>
         </div>
     );
